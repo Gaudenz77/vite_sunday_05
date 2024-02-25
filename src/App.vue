5d407241ee1a004b9799cc5eddb93d36917d861c
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-02-25 21:06:54 +0100

    small changes
    clone 1st omn wsl2

--- src/App.vue
+++ src/App.vue
@@ -35,15 +35,67 @@ import HelloWorld from './components/HelloWorld.vue'
     <!-- Main Content -->
 
       <div class="row">
-        <div class="col-6 p-4">
+        <div class="col-12 col-md-6 p-4 leftCol">
           <!-- Left Column Content -->
-          <h2>Main Section Left Column</h2>
+          <h2 class="px-3">Main Section Left Column</h2>
+          <div class="row">
+            <div class="col-12 mx-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id, accusamus.</div>
+            <div class="col-12">
+              <div class="mb-3">
+                <form class="row g-3 mt-4 px-2 px-md-5">
+                  <div class="col-12 col-md-6">
+                    <label for="inputEmail4" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4">
+                  </div>
+                  <div class="col-12 col-md-6">
+                    <label for="inputPassword4" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4">
+                  </div>
+                  <div class="col-12">
+                    <label for="inputAddress" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                  </div>
+                  <div class="col-12">
+                    <label for="inputAddress2" class="form-label">Address 2</label>
+                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                  </div>
+                  <div class="col-12 col-md-6">
+                    <label for="inputCity" class="form-label">City</label>
+                    <input type="text" class="form-control" id="inputCity">
+                  </div>
+                  <div class="col-md-4">
+                    <label for="inputState" class="form-label">State</label>
+                    <select id="inputState" class="form-select">
+                      <option selected>Choose...</option>
+                      <option>...</option>
+                    </select>
+                  </div>
+                  <div class="col-md-2">
+                    <label for="inputZip" class="form-label">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                  </div>
+                  <div class="col-12">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="gridCheck">
+                      <label class="form-check-label" for="gridCheck">
+                        Check me out
+                      </label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                  </div>
+                </form>
+              </div>
+              
+            </div>
+          </div>
           <!-- Add your content here -->
         </div>
-        <div class="col-12 col-md-6 px-2">
+        <div class="col-12 col-md-6 mb-5 px-2">
           <!-- Right Column Content with Background Image -->
-          <div class="dark-overlay"></div>
-          <img class="col-11 image-fluid mt-5 p-5" src="./assets/spaceflow_office_orig.png" alt="" srcset="">
+          <!-- <div class="dark-overlay"></div> -->
+          <img class="col-12 image-fluid mb-4 p-0 p-md-5" src="./assets/spaceflow_office_orig.png" alt="" srcset="">
           <!-- <h2>Main Section Right Column</h2> -->
           <!-- Add your content here -->
         </div>
